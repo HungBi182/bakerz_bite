348d2dc9bf492d7363d5f9b8572c48a2ff914f48
Long <89914013+long10a10302@users.noreply.github.com>
2024-08-30 19:20:33 +0700

    Long_themgihang

--- Long/bakerz_bite-app/database/migrations/0001_01_01_000000_create_users_table.php
+++ Long/bakerz_bite-app/database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,7 @@ class CreateUsersTable extends Migration
             $table->id(); // Tạo cột khóa chính với tên là `id`
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('role');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
